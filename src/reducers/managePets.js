be110fb89b64b0224c1efb8245e3959b43480dcf
Cara Kane <cara.kane@gmail.com>
2017-09-30 17:20:52 +0000

    automatically backed up by learn

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -17,7 +17,7 @@ export function dispatch(action){
   }
 
 export function render(){
-  document.getElementById("container").innerHTML = '<ul>' + state.pets.map(pet => {<li>{pet.name}</li>}) + '</ul>'
+  document.getElementById("container").innerHTML = '<ul>' + state.pets.map(pet => {'<li>'+ {pet.name} + '</li>'}) + '</ul>'
 }
 
 // '<ul>' +
